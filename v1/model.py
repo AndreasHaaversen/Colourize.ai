bf4e7cc8d80bd4702fd64c5c174225e30b5a1358
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-03-23 16:59:21 +0100

    Restructured repo

--- v1/model.py
+++ v1/model.py
@@ -10,7 +10,7 @@ import os
 import random
 import tensorflow as tf
 
-image = img_to_array(load_img('woman.png'))
+image = img_to_array(load_img('v1/woman.png'))
 image = np.array(image, dtype=float)
 
 X = rgb2lab(1.0/255*image)[:,:,0]
@@ -36,13 +36,22 @@ model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
 
 model.compile(optimizer='rmsprop', loss='mse')
 
-model.fit(x=X, y=Y, batch_size=1, epochs=20)
+model.fit(x=X, y=Y, batch_size=1, epochs=1500)
 print(model.evaluate(X, Y, batch_size=1))
+
+image = img_to_array(load_img('v1/swim.png'))
+image = np.array(image, dtype=float)
+
+X = rgb2lab(1.0/255*image)[:,:,0]
+Y = rgb2lab(1.0/255*image)[:,:,1:]
+Y = Y / 128
+X = X.reshape(1, 400, 400, 1)
+Y = Y.reshape(1, 400, 400, 2)
 
 output = model.predict(X)
 output = output * 128
 canvas = np.zeros((400, 400, 3))
 canvas[:,:,0] = X[0][:,:,0]
 canvas[:,:,1:] = output[0]
-imsave("img_result.png", lab2rgb(canvas))
-imsave("img_grey_scale.png", rgb2gray(lab2rgb(canvas)))
+imsave("v1/img_result.png", lab2rgb(canvas))
+imsave("v1/img_grey_scale.png", rgb2gray(lab2rgb(canvas)))
